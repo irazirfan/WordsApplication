706291f9b4bebf089ab277cc43895f66326f35ed
Iraz Irfan <irazirfan@gmail.com>
2022-06-16 01:10:38 +0600

    saving succesful to db

--- WordsApplication/Models/Word.cs
+++ WordsApplication/Models/Word.cs
@@ -1,7 +1,11 @@
-﻿namespace WordsApplication.Models
+﻿using Newtonsoft.Json;
+
+namespace WordsApplication.Models
 {
     public class Word
     {
+        [JsonProperty]
+        public int Id { get; set; }
         public int Lines { get; set; }
         public string Words { get; set; }
     }
