31eae96ef1166ddf6be421d691b1de6561cdeca4
Iraz Irfan <irazirfan@gmail.com>
2022-06-15 17:50:10 +0600

    WordController init

---
+++ WordsApplication/Controllers/WordController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WordsApplication.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WordController : ControllerBase
+    {
+        private static readonly string[] wordList = new[]
+        {
+            "Basketball", "Cricket", "Football", "Hockey", "Tennis", "Volleyball", "Badminton"
+        };
+
+        private readonly ILogger<WordController> _logger;
+
+        public WordController(ILogger<WordController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string[] Get()
+        {
+            return wordList;
+        }
+    }
+}
